c81c31529b7c5852f4117a9d46a63ac9ee4dbd40
Brendon Scheinman <bscheinman@gmail.com>
2017-01-28 20:14:06 -0800

    fixed heap insertion but still unbalanced

--- src/libcix/heap.rs
+++ src/libcix/heap.rs
@@ -1,7 +1,7 @@
 use std::cell::Cell;
 use std::cmp::{Ord, Ordering};
 use std::fmt;
-use std::fmt::{Display, Formatter};
+use std::fmt::{Debug, Display, Formatter};
 use std::vec::Vec;
 
 // I would prefer to use Option<u32> or something similar here but I don't
@@ -9,7 +9,7 @@ use std::vec::Vec;
 // it because in theory it could hold any value that can fit in 32 bits
 type HeapPtr = i32;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct HeapNodeMd {
     parent:         HeapPtr,
     left_child:     HeapPtr,
@@ -17,11 +17,13 @@ struct HeapNodeMd {
     size:           u32
 }
 
+#[derive(Debug)]
 struct HeapNode<T> where T: Default + Ord{
     value:  T,
     md:     Cell<HeapNodeMd>
 }
 
+#[derive(Debug)]
 pub struct TreeHeap<T> where T: Default + Ord {
     root: HeapPtr,
     pool: Vec<HeapNode<T>>,
@@ -75,7 +77,7 @@ impl<T> TreeHeap<T> where T: Default + Ord {
         let mut heap = TreeHeap {
             root: -1,
             pool: Vec::with_capacity(capacity),
-            free_list: (0..(capacity as i32)).collect()
+            free_list: (0..(capacity as i32)).rev().collect()
         };
 
         for _ in 0..capacity {
@@ -213,39 +215,47 @@ impl<T> TreeHeap<T> where T: Default + Ord {
         };
 
         let head_node = self.get_node_md(head);
-        let mut parent_node = self.get_node_md(parent_index);
-        let mut descend_node = self.get_node_md(descend_index);
 
-        parent_node.size += 1;
+        // XXX: update node sizes appropriately
 
         if head_node.left_child < 0 {
-            parent_node.right_child = head_node.right_child;
-            if parent_node.right_child >= 0 {
+            if head_node.right_child >= 0 {
                 self.get_node_md_mut(head_node.right_child).parent =
                     parent_index;
             }
-            parent_node.left_child = descend_index;
-            descend_node.parent = parent_index;
-            descend_node.left_child = -1;
-            descend_node.right_child = -1;
-            self.set_node_md(parent_index, parent_node);
-            self.set_node_md(descend_index, descend_node);
+            {
+                let parent_node = self.get_node_md_mut(parent_index);
+                parent_node.right_child = head_node.right_child;
+                parent_node.left_child = descend_index;
+            }
+            {
+                let descend_node = self.get_node_md_mut(descend_index);
+                descend_node.parent = parent_index;
+                descend_node.left_child = -1;
+                descend_node.right_child = -1;
+                descend_node.size = 0;
+            }
             return parent_index;
         }
 
         if head_node.right_child < 0 {
-            parent_node.left_child = head_node.left_child;
             self.get_node_md_mut(head_node.left_child).parent = parent_index;
-            parent_node.right_child = descend_index;
-            descend_node.parent = parent_index;
-            descend_node.left_child = -1;
-            descend_node.right_child = -1;
-            self.set_node_md(parent_index, parent_node);
-            self.set_node_md(descend_index, descend_node);
+            {
+                let parent_node = self.get_node_md_mut(parent_index);
+                parent_node.left_child = head_node.left_child;
+                parent_node.right_child = descend_index;
+            }
+            {
+                let descend_node = self.get_node_md_mut(descend_index);
+                descend_node.parent = parent_index;
+                descend_node.left_child = -1;
+                descend_node.right_child = -1;
+                descend_node.size = 0;
+            }
             return parent_index;
         }
 
-        let child_index = {
+        let go_left = {
             let left_size = {
                 let left_node = self.get_node_md_mut(head_node.left_child);
                 left_node.parent = parent_index;
@@ -258,33 +268,42 @@ impl<T> TreeHeap<T> where T: Default + Ord {
                 right_node.size
             };
 
-            if left_size <= right_size {
-                head_node.left_child
-            } else {
-                head_node.right_child
-            }
+            left_size < right_size
         };
 
-        self.insert_node(child_index, descend_index)
+        let child_index = if go_left {
+            head_node.left_child
+        } else {
+            head_node.right_child
+        };
+
+        let new_child = self.insert_node(child_index, descend_index);
+        let parent_node = self.get_node_md_mut(parent_index);
+        if go_left {
+            parent_node.left_child = new_child;
+            parent_node.right_child = head_node.right_child;
+        } else {
+            parent_node.left_child = head_node.left_child;
+            parent_node.right_child = new_child;
+        }
+        parent_node.parent = head_node.parent;
+
+        parent_index
     }
 
-    // XXX: only implement this if T: Copy
-    // That way we can copy out the node's value and safely reuse the node
-    /*
-    pub fn pop(&mut self) -> HeapPtr {
+    pub fn pop(&mut self) -> T where T: Copy {
         assert!(self.root >= 0);
         let head_index = self.root;
-        let &head = self.get(head_index);
+        let head = self.get_node_md(head_index);
         self.root = self.pull_up(head.left_child,
                                  head.right_child);
         if self.root >= 0 {
-            let &mut new_head = self.get_mut(self.root);
-            new_head.parent = -1;
+            let root = self.root;
+            self.get_node_md_mut(root).parent = -1;
         }
 
-        head
+        self.get_node(head_index).value
     }
-    */
 
     pub fn insert<F>(&mut self, ctor: F) -> Result<HeapHandle, &'static str>
             where F: Fn(&mut T) {
--- src/test/heap_test.rs
+++ src/test/heap_test.rs
@@ -3,13 +3,24 @@ extern crate libcix;
 use libcix::heap;
 
 fn main() {
-    let mut h = heap::TreeHeap::new(256);
+    let mut h = heap::TreeHeap::new(16);
     println!("{}", h);
 
     //for x in vec![5u32, 1u32, 10u32] {
     for x in 0..10u32 {
         let _ = h.insert(|v| { *v =  x; });
+        println!("added {}", x);
+        println!("new heap contents:");
+        println!("{}", h);
+        //println!("{:?}", h);
     }
 
     println!("{}", h);
+
+    while !h.is_empty() {
+        let v = h.pop();
+        println!("popped {}", v);
+        println!("new heap contents:");
+        println!("{}", h);
+    }
 }
